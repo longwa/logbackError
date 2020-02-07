c4bca184ece9f84e05c241ee165b503cb6d3f453
Aaron Long <along@triu.com>
2020-02-07 16:19:38 -0500

    Simplify app

--- grails-app/init/test/Application.groovy
+++ grails-app/init/test/Application.groovy
@@ -2,25 +2,11 @@ package test
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-
 import groovy.transform.CompileStatic
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.ldap.LdapAutoConfiguration
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.scheduling.annotation.EnableAsync
 
-@ComponentScan(value = 'com.triu')
-@EnableAsync(proxyTargetClass = true)
-@EnableAutoConfiguration(exclude = [LdapAutoConfiguration])
+@CompileStatic
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
-    }
-
-    @Override
-    Closure doWithSpring() {
-        { ->
-            foo(TestService)
-        }
     }
 }
--- plugin/grails-app/conf/logback.groovy
+++ plugin/grails-app/conf/logback.groovy
@@ -1,96 +1,37 @@
-//
-// See http://logback.qos.ch/manual/groovy.html for details on configuration
-//
-import ch.qos.logback.classic.Level
-import ch.qos.logback.classic.PatternLayout
-import ch.qos.logback.classic.filter.ThresholdFilter
-import ch.qos.logback.core.util.FileSize
 import grails.util.BuildSettings
 import grails.util.Environment
-import grails.util.Metadata
 import org.springframework.boot.logging.logback.ColorConverter
 import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
 
-import java.nio.charset.Charset
-
-statusListener(NopStatusListener)
+import java.nio.charset.StandardCharsets
 
 conversionRule 'clr', ColorConverter
 conversionRule 'wex', WhitespaceThrowableProxyConverter
 
-scan("30 seconds")
-
-def APP_NAME = Metadata.current.getApplicationName()
-def APP_VERSION = Metadata.current.getApplicationVersion()
-def LOG_HOME = System.getProperty("log.home") ?: "${BuildSettings.BASE_DIR}/logs"
-
+// See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
-    filter(ThresholdFilter) {
-        level = INFO
-    }
     encoder(PatternLayoutEncoder) {
-        charset = Charset.forName('UTF-8')
+        charset = StandardCharsets.UTF_8
+
         pattern =
-            '%clr(%d{HH:mm:ss.SSS}){faint}: ' + // Date
-                '%clr(%5p) - ' + // Log level
-                '%clr(%-30.30logger{29}){cyan} %clr(:){faint} ' + // Logger
-                '%m%n%wex' // Message
+                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+                        '%clr(%5p) ' + // Log level
+                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                        '%m%n%wex' // Message
     }
 }
 
-def createFileAppender = { String name, String suffix, Level threshold, Integer history ->
-    appender(name, RollingFileAppender) {
-        file = "${LOG_HOME}/${APP_NAME}-${suffix}.log"
+def targetDir = BuildSettings.TARGET_DIR
+if (Environment.isDevelopmentMode() && targetDir != null) {
+    appender("FULL_STACKTRACE", FileAppender) {
+        file = "${targetDir}/stacktrace.log"
+        append = true
         encoder(PatternLayoutEncoder) {
-            charset = Charset.forName('UTF-8')
-            pattern = '%d{yyyy-MM-dd HH:mm:ss.SSS} ' + // Date
-                '[%5p] ' + // Log level
-                '%logger{29} : ' + // Logger
-                '%m%n%wex' // Message
-        }
-        filter(ThresholdFilter) {
-            level = threshold
-        }
-        rollingPolicy(TimeBasedRollingPolicy) {
-            maxHistory = history
-            fileNamePattern = "${LOG_HOME}/archive/${APP_NAME}-${suffix}.%d.log"
-            if (Environment.isDevelopmentMode()) {
-                totalSizeCap = FileSize.valueOf("500MB")
-            }
+            charset = StandardCharsets.UTF_8
+            pattern = "%level %logger - %msg%n"
         }
     }
+    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
 }
-
-createFileAppender("TF_INFO",  "info",  INFO,  30)
-createFileAppender("TF_ERROR", "error", ERROR, 90)
-
-// These loggers are enabled by default in all modes
-def loggedPackages = ['groovy.grails', 'com.triu', 'common', 'trifleet', 'liquibase', 'com.agileorbit.schwartz.listener']
-
-logger("org.hibernate.cache.ehcache.AbstractEhcacheRegionFactory", OFF)
-
-if (Environment.current == Environment.PRODUCTION && !Boolean.getBoolean('add.common.config')) {
-    loggedPackages.each {
-        logger(it, INFO, ['STDOUT', 'TF_INFO', 'TF_ERROR'], false)
-    }
-
-    root(ERROR, ['STDOUT'])
-}
-else {
-    createFileAppender("TF_SQL", "sql", TRACE, 5)
-    createFileAppender("TF_DEBUG", "debug", DEBUG, 5)
-    createFileAppender("TF_CONSOLE", "console", INFO, 5)
-
-    loggedPackages.each {
-        logger(it, DEBUG, ['STDOUT', 'TF_INFO', 'TF_ERROR', 'TF_DEBUG'], false)
-    }
-
-    // SQL Logging
-    logger("org.hibernate.SQL", DEBUG, ["TF_SQL"], false)
-    logger("org.hibernate.type.descriptor.sql.BasicBinder", TRACE, ["TF_SQL"], false)
-    logger("org.hibernate.engine.internal", OFF)
-    logger("org.hibernate.orm.deprecation", OFF)
-    logger("org.hibernate.tool.schema.internal.ExceptionHandlerLoggedImpl", ERROR)
-
-    root(INFO, ['STDOUT', 'TF_CONSOLE'])
-}
+root(ERROR, ['STDOUT'])
